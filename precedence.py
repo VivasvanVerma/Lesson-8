312353b56ff11ba56b3826d69950312128a439a1
VivasvanVerma <vvofficial.yt@gmail.com>
2025-04-12 15:11:17 +0530

    precedence

---
+++ precedence.py
@@ -0,0 +1,14 @@
+a = 4
+b = 5
+c = 3
+d = 6 
+e = 0
+e = (a+b) * d**c
+print(e)
+
+name = "Alex"
+age = 0
+if name == "Alex" or name == "John" and age >= 0:
+    print("Hello")
+else:
+    print("Goodbye")
