be1f8f453b2b92174640ce5af3a24a251d2fd078
VivasvanVerma <vvofficial.yt@gmail.com>
2025-04-12 15:35:18 +0530

    slow

--- slow.py
+++ slow.py
@@ -1,6 +1,6 @@
-a = 10
-b = 20
-c = 30
+a = int(input("Enter a speed: "))
+b = int(input("Enter another speed: "))
+c = int(input("Enter another speed: "))
 avg = (a + b + c)/3
 if a<avg:
     print("a is slower than the average speed")
