3af08d18a976be8ca90b8b847631b44e61eb20f4
VivasvanVerma <vvofficial.yt@gmail.com>
2025-04-12 15:33:14 +0530

    slow

---
+++ slow.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+c = 30
+avg = (a + b + c)/3
+if a<avg:
+    print("a is slower than the average speed")
+elif b<avg:
+    print("b is slower than the average speed")
+elif c<avg:
+    print("c is slower than the average speed")
+else:
+    print("no one is slower than the average speed")
